6fb0ceaa126ca12b23ccd09c581fc429f7af3dcf
Amritanshu Sikdar <amritanshu.sikdar@gmail.com>
2022-11-19 17:04:23 +0100

    update generate-react-cli config

---
+++ src/components/Damnson/Damnson.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+interface DamnsonProps {}
+
+const Damnson: FC<DamnsonProps> = () => (
+  <div data-testid="Damnson">
+    Damnson Component
+  </div>
+);
+
+export default Damnson;
