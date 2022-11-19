8dcb9ff61cc1029fe4cf84528dd6c9c54274803c
Amritanshu Sikdar <amritanshu.sikdar@gmail.com>
2022-11-19 21:38:57 +0100

    add DocumentModal

--- src/components/Home/Documents/Documents.tsx
+++ src/components/Home/Documents/Documents.tsx
@@ -1,5 +1,17 @@
-import React, { FC } from "react";
-import { IonCard, IonIcon, IonItemDivider, IonTitle } from "@ionic/react";
+import React, { FC, useRef } from "react";
+import {
+    IonButton,
+    IonButtons,
+    IonCard,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonItemDivider,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    useIonModal,
+} from "@ionic/react";
 import {
     documentLock as DocumentLockIcon,
     document as DocumentIcon,
@@ -20,15 +32,25 @@ const documentsList = [
     "Overall Performance",
 ];
 
-export const Documents: FC<DocumentsProps> = () => (
-    <div data-testid="Documents">
-        {documentsList.map((document, index) => (
-            <IonItemDivider key={index} style={{ padding: 0 }}>
-                <DocumentCard title={document} locked={true} />
-            </IonItemDivider>
-        ))}
-    </div>
-);
+export const Documents: FC<DocumentsProps> = () => {
+    const [present, dismiss] = useIonModal(DocumentModal, {
+        onDismiss: () => dismiss(),
+    });
+
+    return (
+        <div data-testid="Documents">
+            {documentsList.map((document, index) => (
+                <IonItemDivider
+                    key={index}
+                    style={{ padding: 0 }}
+                    onClick={() => present()}
+                >
+                    <DocumentCard title={document} locked={true} />
+                </IonItemDivider>
+            ))}
+        </div>
+    );
+};
 
 const DocumentCard: FC<DocumentCardProps> = (props) => {
     return (
@@ -40,6 +62,29 @@ const DocumentCard: FC<DocumentCardProps> = (props) => {
             )}
             <IonTitle>{props.title}</IonTitle>
         </IonCard>
+    );
+};
+
+const DocumentModal: FC<any> = ({ onDismiss }: { onDismiss: () => void }) => {
+    return (
+        <IonPage style={{ textAlign: "center" }}>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Action Required</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <div className="document-modal-content">
+                    <h4>The selected document is locked. Mint it to unlock.</h4>
+                    <div className={"document-modal-container-btn"}>
+                        <IonButton onClick={() => onDismiss()}>
+                            Cancel
+                        </IonButton>
+                        <IonButton onClick={() => onDismiss()}>Mint</IonButton>
+                    </div>
+                </div>
+            </IonContent>
+        </IonPage>
     );
 };
 
