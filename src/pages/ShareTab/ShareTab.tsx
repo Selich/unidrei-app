68a899028a6db8de474a6cdec1850f006bc87b92
Amritanshu Sikdar <amritanshu.sikdar@gmail.com>
2022-11-20 01:37:06 +0100

    change text align back

--- src/pages/ShareTab/ShareTab.tsx
+++ src/pages/ShareTab/ShareTab.tsx
@@ -21,7 +21,7 @@ const ShareTab: React.FC = () => {
                 <Profile />
                 <div className={"share-tab-content"}>
                     <h3>An Authentic Way to Present Yourself</h3>
-                    <h6 className={"ion-text-justify"}>
+                    <h6>
                         This QR-Code leads to your currently connected wallet
                         address. Anybody who scans this, would be able to see
                         your publicly shared, i.e., minted documents.
